50fb4328966234bb8dada440aa48a8a5d21fdd13
zTrix <i@ztrix.me>
2011-07-08 06:46:47 +0800

    passed step 3 4

--- yfs_client.cc
+++ yfs_client.cc
@@ -2,6 +2,7 @@
 #include "yfs_client.h"
 #include "extent_client.h"
 #include "lock_client.h"
+#include "lock_client_cache.h"
 #include <sstream>
 #include <iostream>
 #include <stdio.h>
@@ -15,7 +16,7 @@
 yfs_client::yfs_client(std::string extent_dst, std::string lock_dst)
 {
     ec = new extent_client(extent_dst);
-    lc = new lock_client(lock_dst);
+    lc = new lock_client_cache(lock_dst);
     srand(time(NULL) + getpid());
 }
 
